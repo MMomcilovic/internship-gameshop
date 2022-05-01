8626325ce70ea86e7419833af0c53150d74ed79d
MMomcilovic <61472289+MMomcilovic@users.noreply.github.com>
2022-05-01 16:07:15 +0200

    Promena modela da podrzava selektivni i globalni popust #4

--- GameShop/GameShop/data/Data.cs
+++ GameShop/GameShop/data/Data.cs
@@ -12,7 +12,8 @@ namespace GameShop.data
     {
         public event PropertyChangedEventHandler? PropertyChanged;
         public List<Product> products { get; set; } = new List<Product>();
-
+        public double GlobalTax { get; set; } = 20;
+        public double GlobalDiscount { get; set; } = 0;
         internal Product ProductWithUPC(int UPC)
         {
             foreach(Product p in products)
--- GameShop/GameShop/model/Product.cs
+++ GameShop/GameShop/model/Product.cs
@@ -13,25 +13,35 @@ namespace GameShop.model
         public string name { get; set; }
         public int UPC { get; set; }
         public double price { get; set; }
-        public double tax { get; set; } = 20;
+        public double tax { get; set; }
         public double discount { get; set; } = 0;
 
-        public double GetTaxAmount()
+        public double GetTaxAmount(double GlobalT)
         {
-            return price * tax / 100;
+            double localT = price * tax / 100;
+            double globalT = price * GlobalT / 100;
+            return localT + globalT;
         }
-        public double GetDiscountAmount()
+        public double GetDiscountAmount(double GlobalDiscount)
         {
-            return price * discount / 100;
+            double disc = GlobalDiscount + discount;
+            if (disc > 100) disc = 100;
+            return price * disc / 100;
 
         }
-        private object GetFullPrice()
+        private object GetFullPrice(double globalD, double GlobalT)
         {
-            return price + GetTaxAmount() - GetDiscountAmount();
+            return price + GetTaxAmount(GlobalT) - GetDiscountAmount(globalD);
         }
-        internal string FullPriceDisplay()
+        internal string FullPriceDisplay(double GlobalTax, double GlobalDisocunt)
         {
-            return $"Iznos poreza = {GetTaxAmount():N2} din ({tax:N2}%); Iznos popusta = {GetDiscountAmount():N2} din ({discount}%)\nOsnova cena {price:N2} din, Cena nakon popusta i poreza {GetFullPrice():N2} din";
+            string s = $"Iznos poreza = {(price * GlobalTax / 100):N2} din ({GlobalTax:N2}%); Ukupan popust = {GetDiscountAmount(GlobalDisocunt):N2} din ({GlobalDisocunt+discount}%)\n";
+            if (discount > 0)
+            {
+                s += $"Proizvod sadrzi selektivni popust ({discount}%)\n";
+            }
+            s += $"Osnovna cena {price:N2} din, Cena nakon popusta i poreza {GetFullPrice(GlobalDisocunt, GlobalTax):N2} din";
+            return s;
         }
 
     }
